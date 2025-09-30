94bd21cd6258890ec097bce4a2b3511b849dee7b
Seizedx <mapedlow@hotmail.com>
2025-09-30 20:40:38 -0300

    g

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,20 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+
+////////////////////// NAO ESTAVA CONECTANDO, FIZ ISSO:
+
+
+    // rm -rf .expo
+    // rm -rf ios
+    // rm -rf android
+    // eas build:configure --platform android
+
+
+// // Configura o Metro para usar o IP da sua máquina
+// config.server = {
+//   ...config.server,
+//   host: '192.168.15.143', // Substitua pelo IP da sua máquina
+// };
+
 module.exports = config;
